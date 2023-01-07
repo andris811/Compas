0415968c75d3ff383731a316da2785af62597b62
Andras Varga <vargan89@gmail.com>
2023-01-07 14:51:17 +0800

    feat: routes for trips, trips controller, trips views skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/home'
+
+  resources :trips
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
