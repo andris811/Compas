05391ec8c4097ac9262ba957fa685a606e36a7eb
john stowers <stowers.elliot@gmail.com>
2023-01-07 18:11:16 +0800

    second draft completed. Styles and tags add. Navbar integrated, search function begun.

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,9 +1,9 @@
 import "./main.scss";
 
-import { createApp } from 'vue';
+import { createApp } from "vue";
 import Home from "../components/views/Home.vue";
 
-if (document.querySelector('#home')) {
+if (document.querySelector("#home")) {
   const home = createApp(Home);
-  home.mount('#home');
+  home.mount("#home");
 }
