7cdb3db3bed21fedfcca8743a526fef50c98130a
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-01-14 12:53:11 +0800

    bugfixing vue

--- app/frontend/components/views/Quiz.vue
+++ app/frontend/components/views/Quiz.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>Hello from quiz</h1>
+  <div class="my-10 container w-full p-5 text-center">
+    <div class="flex flex-row w-auto h-max">
+      <div class="basis-1/3 h-96 bg-cover bg-center rounded-l-xl bg-[url('/images/quiz/globe.jpeg')]"></div>
+      <div class="basis-2/3 bg-darkblue rounded-r-xl"></div>
+    </div>
   </div>
 </template>
 
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,10 +1,20 @@
 import "./main.scss";
 
-import { createApp } from "vue";
-// import Home from "../components/views/Home.vue";
-import Quiz from "../components/views/Quiz.vue";
+// import { createApp } from "vue";
+// // import Home from "../components/views/Home.vue";
+// import Quiz from "../components/views/Quiz.vue";
 
-if (document.querySelector("#quiz")) {
-  const quiz = createApp(Quiz);
-  quiz.mount("#quiz");
-}
+// if (document.querySelector("#quiz")) {
+//   const quiz = createApp(Quiz);
+//   quiz.mount("#quiz");
+// }
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+import Home from "@/components/views/Home.vue";
+import Quiz from "@/components/views/Quiz.vue";
+
+const app = createApp();
+
+app.component("Home", Home);
+app.component("Quiz", Quiz);
+app.mount("#vue");
