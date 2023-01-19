a052554685c7d2ab94492291d27274e4d5a5e891
john stowers <stowers.elliot@gmail.com>
2023-01-19 18:05:32 +0800

    Removed nav and foot from landing

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "simple_calendar", "~> 2.4"
 
 gem "dotenv-rails", groups: [:development, :test]
 
+gem "cloudinary"
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @nav = false
+    @foot = false
     if user_signed_in?
       redirect_to trips_path
     end
