023d335d8e330e09f0b64bd08ae09638172251af
Andras Varga <vargan89@gmail.com>
2023-01-21 11:20:32 +0800

    seeded trips with images, avatars, show page updates if logged in or not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_154007) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_021151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,7 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_154007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "activities"
-    t.string "trip_img", default: [], array: true
+    t.string "trip_img", default: "{}"
     t.string "location"
     t.string "country"
     t.index ["user_id"], name: "index_trips_on_user_id"
