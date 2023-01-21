b81712ff0e3dbec95bd423309dc75806cb0c3f5c
Andras Varga <vargan89@gmail.com>
2023-01-21 15:56:37 +0800

    fixed after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     trips_path
   end
+
+  def after_sign_in_path_for(resource)
+    quizzes_path
+  end
 end
