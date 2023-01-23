e35373d4673b32619287663757269620e7387b9c
john stowers <stowers.elliot@gmail.com>
2023-01-23 12:17:14 +0800

    did seem to push last time

--- db/seeds.rb
+++ db/seeds.rb
@@ -310,8 +310,6 @@ puts 'Creating trips'
     max_people: Faker::Number.between(from: 1, to: 30),
     description: Faker::Quote.matz,
     pets: Faker::Boolean.boolean,
-    start_date: Faker::Date.forward(days: 7),
-    end_date: Faker::Date.forward(days: 20),
     user_id: users.sample.id,
     activities: activities.sample(rand(1..4)),
     country: cnt,
