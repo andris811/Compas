f5fad908607c86cebb4008260d89ad3cd743c3fa
Andras Varga <vargan89@gmail.com>
2023-02-09 20:28:04 +0800

    users description fix

--- db/migrate/20230207111058_remove_description_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDescriptionFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :description, :text
-    add_column :users, :description, :text, default: ''
-  end
-end
---
+++ db/migrate/20230209122623_remove_description_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveDescriptionFromUsers < ActiveRecord::Migration[7.0]
+  def change
+      remove_column :users, :description, :text
+      add_column :users, :description, :text, default: ''
+  end
+end
